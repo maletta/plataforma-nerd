aaf340b94835e745ad7a3c28985cbe22c12f5a78
Mauricio Maletta <mauricio_maletta@hotmail.com>
2021-01-15 05:50:24 -0300

    add meta tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import Head from 'next/head';
 import Main from 'components/Main';
 import Title from 'components/Title';
 import VideoContainer from 'components/VideoContainer';
@@ -14,11 +15,29 @@ const Index = styled.div`
 
 export default function Home() {
   return (
-    <Index>
-      <Main>
-        <Title />
-        <VideoContainer />
-      </Main>
-    </Index>
+    <>
+      <Head>
+        <title>Arya Gameplays</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:site_name" content="@camilahipolito" />
+        <meta name="og:title" property="og:title" content="Arya GamePlays" />
+        <meta property="og:type" content="website" />
+        <meta name="description" content="Nbesq corporation" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image" content="https://plataforma-nerd.vercel.app/nbesq.png" />
+        <meta property="og:image:type" content="image/jpg" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="Arya GamePlays" />
+        <meta name="twitter:text:title" content="@camilahipolito" />
+        <meta name="twitter:description" content="Nbesq corporation" />
+        <meta name="twitter:image" content="https://plataforma-nerd.vercel.app/nbesq.png" />
+      </Head>
+      <Index>
+        <Main>
+          <Title />
+          <VideoContainer />
+        </Main>
+      </Index>
+    </>
   );
 }
